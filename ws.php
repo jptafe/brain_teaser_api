949f017e7be5355dc27d8239ec5c4a5973715187
Frag Spawn <frag.spawn@gmail.com>
2021-08-10 12:44:05 +1000

    select all done

--- ws.php
+++ ws.php
@@ -1,21 +1,20 @@
 <?php
 
     // we need some setup lines here:
+    $dbURI = 'mysql:host=127.0.0.1;port=3306;dbname=todoapp';
+    $dbconn = new PDO($dbURI, 'databaseuser', 'somesecurepassword');
 
     switch($_GET['page']) {
         case 'insert':
             // check to see if its a post, send all post data to a db function()
             if (db_insert() == true) {
                 http_response_code(201);
-                // do something
             } else {
                 http_response_code(400);
-                // do something else
             }
             break;
         case 'update':
             // check to see if its a post, send all post data to a db function()
-            http_response_code(202);
             if (db_update() == true) {
                 http_response_code(201);
             } else {
@@ -40,12 +39,12 @@
             }               
             break;
         case 'selectall':
-            // no tests required, just pump out a result from db function()
-            if (db_select_all() == true) {
+            $res = db_select_all();
+            if (is_array($res)) {
                 http_response_code(200);
-                echo json_encode(Array('msg'=>'roger selectall red leader'));
+                echo json_encode($res);
             } else {
-                http_response_code(400);
+                http_response_code(404);
             }               
             break;
         default:
@@ -76,7 +75,13 @@
     }
 
     function db_select_all() {
+        global $dbconn;
         $sql = "SELECT * FROM todouser";
+        $stmt = $dbconn->prepare($sql);
+        $stmt->execute();
+        if($stmt->rowCount() > 0) {
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }
         return false;
     }
 ?>
